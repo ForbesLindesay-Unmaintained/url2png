fc3428ad57cbd1b30520c96c1e8eb6b26591245e
apank <annavpankevich@gmail.com>
2016-10-31 05:42:44 -0400

    Added ability to use beta api, intergrated 5 options

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ var request = require('request');
 
 module.exports = url2png;
 
-function url2png(apiKey, privateKey) {
+function url2png(apiKey, privateKey, type) {
     var prefix = '//api.url2png.com/v6/';
     var format = 'png'
     var lib = {};
+
+    if(type == 'beta'){
+        prefix = '//beta.url2png.com/v6/';
+    }
 
     function buildURL(url, options) {
         options = options || {};
@@ -17,6 +21,11 @@ function url2png(apiKey, privateKey) {
         if(options.delay && typeof options.delay !== 'number') throw new Error('delay should be a number in seconds');
         if(options.force && typeof options.force !== 'boolean') throw new Error('force should be a boolean');
         if(options.protocol && options.protocol != 'https' && options.protocol != 'http') throw new Error('protocol should either be "https" or "http"');
+        if(options.user_agent && typeof options.user_agent !== 'string') throw new Error('user_agent should be user agent string');
+        if(options.unique && typeof options.unique !== 'string') throw new Error('unique should be a string');
+        if(options.accept_languages && typeof options.accept_languages !== 'string') throw new Error('accept_languages should be a string');
+        if(options.ttl && typeof options.ttl !== 'number') throw new Error('ttl should be a number');
+        if(options.custom_css_url && typeof options.custom_css_url !== 'string') throw new Error('custom_css_url should be a string');
         options.protocol = options.protocol || '';
 
         url = 'url=' + encodeURIComponent(url);
